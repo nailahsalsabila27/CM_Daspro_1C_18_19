68ac9e570caa2b2e7e3bab39c362cfd0fce21292
nailahsalsabila27 <nailahslsb@gmail.com>
2024-12-10 15:23:21 +0700

    tambahstok_alsa

--- src/Tugas1819/CM_Daspro_18_19.java
+++ src/Tugas1819/CM_Daspro_18_19.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 
 public class CM_Daspro_18_19 {
-    
     static Scanner nalalsa = new Scanner(System.in);
     public static String[][] menuKategori = new String[10][2];
+    static int[] stok = new int[10];
     public static void main(String[] args) {
         
         while (true) {
@@ -20,21 +20,42 @@ public class CM_Daspro_18_19 {
             int menu = nalalsa.nextInt();
     
             if (menu == 1) {
-            //    tampilInventori();
+             tampilInventori(menuKategori, stok);
             } else if (menu == 2) {
-        
+              tambahStok();;
             } else if (menu == 3){
-                tambahItem();
+              tambahItem();
             } else if (menu == 4) {
               break;  
             }
         }
     }
+    
+    public static void tampilInventori(String[][] menuKategori, int[] stok) {
+     
+    }
+    
+    public static void tambahStok() {
+      System.out.println("===== TAMBAH STOK =====");
+      System.out.print("Masukkan nomor item: ");
+      int nomor = nalalsa.nextInt();
+      nalalsa.nextLine();
+      if (nomor < 1 || nomor > menuKategori.length || menuKategori[nomor - 1][0] == null) {
+          System.out.println("Item tidak ditemukan.");
+          return;
+        }
+      System.out.print("Masukkan jumlah stok yang ingin ditambahkan: ");
+      int jumlah = nalalsa.nextInt();
+      if (jumlah > 0) {
+        stok[nomor - 1] += jumlah;
+        System.out.println("Stok " + menuKategori[nomor - 1][0] + " berhasil ditambah. Stok sekarang: " + stok[nomor - 1]);
+      }
+    }
+    
 
     public static void tambahItem () {
-        
+    
 
     }
-
     
     }
